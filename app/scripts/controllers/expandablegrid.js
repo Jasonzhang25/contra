c26353408fe4f3961d4656ddc0ab6945d3505974
JasonZhang <wen.zhang3@hpe.com>
2016-10-27 09:52:41 +0800

    add file

--- app/scripts/controllers/expandablegrid.js
+++ app/scripts/controllers/expandablegrid.js
@@ -9,21 +9,53 @@ angular.module('contraApp')
       }
  
       $scope.gridOptions.columnDefs = [
-        { name: 'id', pinnedLeft:true, width:100 },
+        { name: 'id', field:"id", pinnedLeft:true, width:100 },
+        { name: 'guid', width:100},   
+        { name: 'isActive', width:100}, 
+        { name: 'balance', width:100}, 
+        { name: 'picture', width:100},  
+        { name: 'gender', width:100},
+        { name: 'company', width:100},
+        { name: 'email', width:100},                                                    
         { name: 'name', width:100},
         { name: 'age', width:100},
-        { name: 'address.city'}
+        { name: 'company', width:100},
+        { name: 'email', width:100},                                                    
+        { name: 'name', width:100},
+        { name: 'age', width:100},
+        { name: 'phone', width:100}, 
+        { name: 'address.street', width:100}, 
+        { name: 'address.state', width:100}, 
+        { name: 'address.zip', width:100},                                    
+        { name: 'address.city', width:100}
       ];
  
       $http.get('/data/500_complex.json')
         .success(function(data) {
           for(var i = 0; i < data.length; i++){
             data[i].subGridOptions = {
+            enableVerticalScrollbar: false,
+            enableHorizontalScrollbar: false,
               columnDefs: [ 
-              {name:"Id", field:"id", pinnedLeft:true, width:100},
-              {name:"Name", width:100},
-              { name: 'age', width:100},
-              { name: 'address.city'}
+              { name:"Id", field:"id", width:50},
+        { name: 'guid', width:50},   
+        { name: 'isActive', width:100}, 
+        { name: 'balance', width:100}, 
+        { name: 'picture', width:100},  
+        { name: 'gender', width:100},
+        { name: 'company', width:100},
+        { name: 'email', width:100},                                                    
+        { name: 'name', width:100},
+        { name: 'age', width:100},
+        { name: 'company', width:100},
+        { name: 'email', width:100},                                                    
+        { name: 'name', width:100},
+        { name: 'age', width:100},
+        { name: 'phone', width:100}, 
+        { name: 'address.street', width:100}, 
+        { name: 'address.state', width:100}, 
+        { name: 'address.zip', width:100},                                    
+        { name: 'address.city',width:100}
 
               ],
               data: data[i].friends
