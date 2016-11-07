0540ff05ab6fe004d2798a44464c7a8d94633e94
JasonZhang <wen.zhang3@hpe.com>
2016-11-07 10:33:49 +0800

    20161107

--- app/scripts/controllers/expandablegrid.js
+++ app/scripts/controllers/expandablegrid.js
@@ -9,7 +9,18 @@ angular.module('contraApp')
       }
  
       $scope.gridOptions.columnDefs = [
-        { name: 'id', field:"id", pinnedLeft:true, width:100 },
+        { name: 'id', field:"id", width:100 },
+        { name: 'id1', field:"id1",  width:100 },
+        { name: 'id2', field:"id2",  width:100 },
+        { name: 'id3', field:"id3",  width:100 },
+        { name: 'id4', field:"id4",  width:100 },
+        { name: 'id5', field:"id5",  width:100 },
+        { name: 'id6', field:"id6",  width:100 },
+        { name: 'id7', field:"id7",  width:100 },  
+        { name: 'id8', field:"id8",  width:100 },
+        { name: 'id9', field:"id9",  width:100 },
+        { name: 'id10', field:"id10",  width:100 },
+        { name: 'id11', field:"id11",  width:100 },                                     
         { name: 'guid', width:100},   
         { name: 'isActive', width:100}, 
         { name: 'balance', width:100}, 
@@ -28,6 +39,9 @@ angular.module('contraApp')
         { name: 'address.state', width:100}, 
         { name: 'address.zip', width:100},                                    
         { name: 'address.city', width:100}
+        
+       
+
       ];
  
       $http.get('/data/500_complex.json')
@@ -38,7 +52,18 @@ angular.module('contraApp')
             enableHorizontalScrollbar: false,
               columnDefs: [ 
               { name:"Id", field:"id", width:50},
-        { name: 'guid', width:50},   
+        { name: 'id1', field:"id1",  width:50 },
+        { name: 'id2', field:"id2",  width:100 },
+        { name: 'id3', field:"id3",  width:100 },
+        { name: 'id4', field:"id4",  width:100 },
+        { name: 'id5', field:"id5",  width:100 },
+        { name: 'id6', field:"id6",  width:100 },
+        { name: 'id7', field:"id7",  width:100 },  
+        { name: 'id8', field:"id8",  width:100 },
+        { name: 'id9', field:"id9",  width:100 },
+        { name: 'id10', field:"id10",  width:100 },
+        { name: 'id11', field:"id11",  width:100 },                         
+        { name: 'guid', width:100},   
         { name: 'isActive', width:100}, 
         { name: 'balance', width:100}, 
         { name: 'picture', width:100},  
@@ -56,7 +81,6 @@ angular.module('contraApp')
         { name: 'address.state', width:100}, 
         { name: 'address.zip', width:100},                                    
         { name: 'address.city',width:100}
-
               ],
               data: data[i].friends
             }
